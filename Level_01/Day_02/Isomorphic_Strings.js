8447b0db7f185ebb3e28518fc120aecf65389b25
ksooooza <ksoooza@gmail.com>
2023-04-05 09:45:52 -0400

    Day 02

---
+++ Level_01/Day_02/Isomorphic_Strings.js
@@ -0,0 +1,13 @@
+var isIsomorphic = function(s, t) {
+    if(s.length != t.length)
+        return false;
+    const map1 = [256];
+    const map2 = [256];
+    for(let i = 0; i < s.length; i++){
+        if(map1[s.charAt(i)] != map2[t.charAt(i)])
+            return false;
+        map1[s.charAt(i)] = i + 1;
+        map2[t.charAt(i)] = i + 1;
+    }
+    return true;
+};
